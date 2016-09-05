45ae08293985c7510e5fb3198cf672794a3034c4
Dominik Palo <dominik@palo.sk>
2016-09-05 20:45:52 +0200

    Add simple test

--- index.ts
+++ index.ts
@@ -1,20 +1,42 @@
 import express = require("express");
 import http = require("http");
 import socketIo = require("socket.io");
+import util = require("util");
+import bodyParser = require("body-parser");
+
+import Socket = SocketIO.Socket;
 
 
-var app = express();
-var server = http.createServer(<any>app);
-var io = socketIo(server);
+const app = express();
+const server = http.createServer(<any>app);
+const io = socketIo(server);
+
+let clientSocket: Socket;
+
 
 app.use(express.static(__dirname + '/static'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', () => {
-    console.log('New connection')
+app.post('/', (req, res) => {
+    res.sendStatus(200);
+
+    if (clientSocket) {
+        clientSocket.emit("setPin", req.body);
+    }
+});
+
+io.on('connection', (socket: Socket) => {
+    console.log('New connection');
+    clientSocket = socket;
 });
 
 server.listen(3000, () => {
